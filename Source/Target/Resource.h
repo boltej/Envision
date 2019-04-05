d9868a199c191c46eda02e31c416f60bf7b343b4
boltej <john.bolte@oregonstate.edu>
2019-04-05 15:20:13 -0700

    Add files via upload

---
+++ Source/Target/Resource.h
@@ -0,0 +1,47 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Target.rc
+//
+#define IDD_TARGET                      6000
+#define IDC_METHOD                      6000
+#define IDC_VALUE                       6001
+#define IDD_SAVETO                      6001
+#define IDC_COLDENSITY                  6002
+#define IDC_COLCAPACITY                 6003
+#define IDD_CNAME                       6003
+#define IDC_COLAVAIL                    6004
+#define IDC_COLPERCENTAVAIL             6005
+#define IDC_REPORTPLACEHOLDER           6006
+#define IDC_ADDREPORT                   6007
+#define IDC_DELETEREPORT                6008
+#define IDC_CONSTANTPLACEHOLDER         6009
+#define IDC_ADDCONSTANT                 6010
+#define IDC_DELETECONSTANT              6011
+#define IDC_SCENARIOS                   6012
+#define IDC_ALLOCATIONPLACEHOLDER       6013
+#define IDC_ADDALLOCATION               6014
+#define IDC_DELETEALLOCATION            6015
+#define IDC_PREFERENCESPLACEHOLDER      6016
+#define IDC_ADDPREFERENCE               6017
+#define IDC_DELETEPREFERENCE            6018
+#define IDC_SAVEASXML                   6019
+#define IDC_ADDALLOCATIONSET            6025
+#define IDC_REMOVEALLOCATIONSET         6026
+#define IDC_PATH                        6027
+#define IDC_SAVETOFILE                  6028
+#define IDC_SAVESESSION                 6029
+#define IDC_CNAME                       6030
+#define IDC_BROWSEXML                   6032
+#define IDC_ALLOCATIONID                6033
+#define IDC_ALLOCATIONSETID             6033
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        6005
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         6034
+#define _APS_NEXT_SYMED_VALUE           6000
+#endif
+#endif
