6fd9519b44a8e267d7bf739d73d98783101eb51b
boltej <john.bolte@oregonstate.edu>
2019-04-06 19:44:16 -0700

    Add files via upload

---
+++ Source/ExamplePlugin/ExamplePlugin.cpp
@@ -0,0 +1,16 @@
+// ExamplePlugin.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "ExamplePlugin.h"
+
+//extern "C" EnvExtension* Factory()
+//   { 
+//   return (EnvExtension*) new ExampleModel; 
+//   }
+
+
+
+
+
+
