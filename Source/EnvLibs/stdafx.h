1e7cf33e4f26ce7ef4578b33bd51d4da80947ad4
boltej <john.bolte@oregonstate.edu>
2019-04-07 21:30:50 -0700

    Ultimate Toolbox

--- Source/EnvLibs/stdafx.h
+++ Source/EnvLibs/stdafx.h
@@ -7,10 +7,40 @@
 
 #include "targetver.h"
 
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-// Windows Header Files
-#include <windows.h>
+
+#include "targetver.h"
+
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // some CString constructors will be explicit
+
+// turns off MFC's hiding of some common and often safely ignored warning messages
+#define _AFX_ALL_WARNINGS
+
+#include <afxwin.h>         // MFC core and standard components
+#include <afxext.h>         // MFC extensions
 
 
+#include <afxdisp.h>        // MFC Automation classes
 
-// reference additional headers your program requires here
+
+ // Here, minimal DB support is requested.  No view is chosen.
+
+#ifndef _AFX_NO_OLE_SUPPORT
+#include <afxdtctl.h>           // MFC support for Internet Explorer 4 Common Controls
+#endif
+#ifndef _AFX_NO_AFXCMN_SUPPORT
+#include <afxcmn.h>             // MFC support for Windows Common Controls
+#endif // _AFX_NO_AFXCMN_SUPPORT
+
+#include <afxcontrolbars.h>     // MFC support for ribbons and control bars
+
+
+#include <atlbase.h>
+#include <afxoledb.h>
+#include <atlplus.h>
+
+#ifndef _WIN64
+#include <afxdao.h>			// MFC DAO database classes
+#endif
+
+#include <direct.h>
+#include <afxdlgs.h>
